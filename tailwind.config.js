09e8e5d40dc18402593c6087846e0ed4a3730f14
Robert-10J <65472780+Robert-10J@users.noreply.github.com>
2023-05-20 22:58:43 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html", "grafias.html", "medidas.html"],
+  content: ["index.html", "graficas.html", "medidas.html"],
   theme: {
     extend: {},
   },
