6d5e9a9c64248209fcc235179ece3c08d1aeb1bb
Robert <roberttv17@gmail.com>
2023-05-20 22:43:47 -0600

    medidas de dispersion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html"],
+  content: ["index.html", "graficas.html", "medidas.html"],
   theme: {
     extend: {},
   },
